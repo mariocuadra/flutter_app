11fd4d78c1cba560c24d9f0cb142a2e40792e573
Mario Cuadra Seguel <mario.cuadra@gmail.com>
2020-11-10 19:10:23 -0300

    Apriendo a utilizar la libreria firebase_storage

--- lib/Place/repository/firebase_storage_api.dart
+++ lib/Place/repository/firebase_storage_api.dart
@@ -1,6 +1,21 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:html';
+
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class FirebaseStorageAPI{
+
+  final firebase_storage.Reference _storageReference = firebase_storage.FirebaseStorage.instance.ref();
+
+  Future<firebase_storage.UploadTask> uploadFile (String path) async {
+
+    File firebase_storage.UploadTask storageUploadTask = _storageReference.child(path).putFile(image,"image/jpeg");
+
+
+
+
+
+  }
+
 
 
 }
---
+++ lib/Place/repository/firebase_storage_repository.dart
@@ -0,0 +1,8 @@
+import 'package:firebase/firestore.dart';
+
+class FirebaseStorageRespository{
+
+
+
+
+}
