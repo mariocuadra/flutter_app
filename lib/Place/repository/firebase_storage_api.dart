fd285e20f9d51cab6b54149b760b7f90ff1403db
mario cuadra seguel <mario.cuadra@gmail.com>
2020-11-14 13:21:28 -0300

    acepta los cambios hechos en el trabajo

--- lib/Place/repository/firebase_storage_api.dart
+++ lib/Place/repository/firebase_storage_api.dart
@@ -6,11 +6,12 @@ class FirebaseStorageAPI{
 
   final firebase_storage.Reference _storageReference = firebase_storage.FirebaseStorage.instance.ref();
 
-  Future<firebase_storage.UploadTask> uploadFile (String path) async {
+  Future<firebase_storage.UploadTask> uploadFile (String path, File image) async {
 
-    File firebase_storage.UploadTask storageUploadTask = _storageReference.child(path).putFile(image,"image/jpeg");
-
-
+    if (image == null){
+      print("No file was selected");
+      return null;
+    }
 
 
 
